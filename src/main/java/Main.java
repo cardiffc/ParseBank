fc9762f28b43c64b80c84bd4880ecb6a001947ad
Yury Shchegolev <cardiffc@yandex.ru>
2019-11-19 16:51:48 +0300

    ListExpanseByContractor works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     private static final String movementsFile = "data/movementList.csv";
     public static void main(String[] args) throws IOException {
         List<Transaction> allTransactions = new ArrayList<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
+
         CSVReader parseFile = new CSVReader(new FileReader(movementsFile), ',', '"', '~', 1);
+
         List<String[]> parsedLines = parseFile.readAll();
         for (String[] element : parsedLines)
         {
@@ -26,10 +30,24 @@ public class Main {
             String contractor = preContractor.substring(preContractor.replaceAll("\\\\","/")
                     .lastIndexOf("/") + 1).trim();
 
-            String description = element[5];
+            String description = element[5].trim();
             allTransactions.add(new Transaction(amount, contractor, date, direction, description));
         }
-        allTransactions.stream().filter(element -> element.getDirection().equals(Direction.EXPENSE))
-                .forEach(element -> System.out.println(element.getContractor() + "/" + element.getAmount()));
+        listExpanseByContractor(allTransactions);
+    }
+    private static void listExpanseByContractor(List<Transaction> transactions)
+    {
+        TreeMap<String, BigDecimal> expanseByContractor = new TreeMap();
+        transactions.forEach(transaction -> expanseByContractor.put(transaction.getContractor(), new BigDecimal("0")));
+        for (Transaction trans : transactions)
+        {
+            for (Map.Entry<String, BigDecimal> contr : expanseByContractor.entrySet()) {
+             contr.setValue(contr.getKey().equals(trans.getContractor()) ? contr.getValue().add(trans.getAmount()) : contr.getValue());
+            }
+        }
+        for (Map.Entry<String,BigDecimal> entry : expanseByContractor.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
     }
 }
